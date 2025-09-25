76bf01386d83c0f12881ec5013279cbfadef024b
Kai <dk786161616@gmail.com>
2025-09-25 15:24:10 +0530

    Add Consumer module with Kafka integration and necessary configurations

--- Producer/src/main/java/com/demo/Producer/KafkaProducer.java
+++ Producer/src/main/java/com/demo/Producer/KafkaProducer.java
@@ -1,7 +1,5 @@
 package com.demo.Producer;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
